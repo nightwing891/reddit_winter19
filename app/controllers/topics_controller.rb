0068d544d1e6db2f9fe75145ef593fa05403890b
Henry Doan <henrydoan96@gmail.com>
2019-11-21 19:10:16 -0700

    end of lecture

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,13 +1,64 @@
 class TopicsController < ApplicationController
+  # define @parent 
+  # before_action :set_parent 
+  
+  # private 
+  #   def set_parent
+  #     @parent = Model_name_of_parent.find(params[:parent_id])
+  #   end
+  
+  # Model_name  ->  @parent.children(s)
+  before_action :set_sub
+  before_action :set_topic, only: [:show, :update, :edit, :destroy]
+
   def index
+    @topics = @sub.topics 
   end
 
   def show
   end
 
   def new
+    @topic = @sub.topics.new
+    render partial: 'form'
+  end
+
+  def create 
+    @topic = @sub.topics.new(topic_params)
+    if @topic.save
+      redirect_to [@sub, @topic]
+    else
+      render :new 
+    end
   end
 
   def edit
+    render partial: 'form'
   end
+
+  def update
+    if @topic.update(topic_params)
+      redirect_to [@sub, @topic]
+    else
+      render :edit 
+    end
+  end
+
+  def destroy 
+    @topic.destroy
+    redirect_to sub_topics_path(@sub)
+  end
+
+  private
+    def topic_params
+      params.require(:topic).permit(:name, :body)
+    end
+
+    def set_topic
+      @topic = Topic.find(params[:id])
+    end
+
+    def set_sub
+      @sub = Sub.find(params[:sub_id])
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,22 @@
 Rails.application.routes.draw do
-  root 'subs#index'
+  root 'sub#index'
   # HTTP Protocols
     # get
     # post
     # put 
     # delete
   # get '/subs', to: 'subs#index'
+  # get '/apples', to: 'subs#index'
   # get '/subs', to: 'subs#edit'
   # post '/subs', to: 'subs#create'
+  # delete '/subs', to: 'subs#delete'
 
   resources :subs do
-    resources :topics
+    resources :topics 
+  end
+
+  resources :topics do
+    resources :comments
   end
   # resources :subs, only: [:new, :index]
   # resources :subs, except: [:create, :delete]
